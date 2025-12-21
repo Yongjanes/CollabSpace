f621958ae066733a6c1f54632dbed66f30ec4720
Yongjanes <rohit.pali2512@gmail.com>
2025-12-22 00:59:28 +0530

    write util code , and write standerdize error format and response format

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import { connectDB } from "./db/index.js"
+import { app } from "./app.js"
 
 connectDB()
+
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error : ", error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((error) => {
+    console.log("Error on App : ", error)
+    throw error
+})
