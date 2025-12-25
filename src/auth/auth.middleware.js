370f5d0920ca2a23098ce42f995449f78dcf78ca
Yongjanes <rohit.pali2512@gmail.com>
2025-12-25 20:07:34 +0530

    refactor authentication middleware to streamline token handling and user fetching

--- src/auth/auth.middleware.js
+++ src/auth/auth.middleware.js
@@ -1,46 +1,46 @@
 import { ApiError } from '../utils/ApiError.js'
-import { ApiResponse } from '../utils/ApiResponse.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
 import jwt from 'jsonwebtoken'
+import { User } from '../models/User.js'
 
 const requireAuth = asyncHandler( async (req, _, next) => {
     // 1. Get token
     // 2. Verify token
     // 3. Fetch user
     // 4. Check isActive
-    // 5. Attach req.user
-    // 6. next()
+    // 5. next()
 
     // --------------------------------------------------------------
 
     // 1. Get token
+    const access_token = req.cookies?.access_token || req.headers.authorization?.split(' ')[1]
 
-    const token = req.headers.authorization?.split(' ')[1]
-
-    if (!token) {
+    if (!access_token) {
         throw new ApiError(401, 'Authentication token is missing')
     }
 
 
     // 2. Verify token
-
     let decodedToken = null
     try {
-        decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        decodedToken = jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET)
     } catch (error) {
         throw new ApiError(401, 'Invalid authentication token')
     }
 
     // 3. Fetch user
+    req.user = await User.findById(decodedToken.id).select('-password')
 
-    req.user = { id: decodedToken.id, email: decodedToken.email }
+    if (!req.user) {
+        throw new ApiError(401, 'User not found')
+    }
 
     // 4. Check isActive
-    // Skipping for now
+    if (!req.user.isActive) {
+        throw new ApiError(403, 'User account is inactive')
+    }
 
-    // 5. Attach req.user
-    // Done above
-
-    // 6. next()
+    // 5. next()
     next();
+    
 })
