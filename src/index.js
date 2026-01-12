e4a529ad34b53a2ffc3ddd247d6eaf0c4d6dccd8
Yongjanes <rohit.pali2512@gmail.com>
2026-01-12 19:21:27 +0530

    added auth documentation and also the activity log route and also added pagination query for all paginated routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { connectDB } from "./db/index.js"
 import { app } from "./app.js"
+import { connectDB } from "./db/index.js"
 import { connectRedis } from './config/redis.js'
 
 // connectDB()
